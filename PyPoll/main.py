50f8484bf0f16d706960d765fa267869dfa84b77
brian-halll <bhall31999@gmail.com>
2022-07-03 17:43:59 -0400

    adding pypoll main script

---
+++ PyPoll/main.py
@@ -0,0 +1,63 @@
+#import modules
+import os 
+import csv
+
+# create path for csv
+resource_csv = os.path.join("Resources", "election_data.csv")
+
+totalVotes = 0
+StockhamVotes = 0
+DegetteVotes = 0
+DoaneVotes = 0
+winnerVotes = 0
+winner = ""
+
+with open(resource_csv) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+
+    # grab header
+    header = next(csv_reader)
+
+    
+
+    for row in csv_reader:
+        totalVotes = totalVotes + 1
+
+        if row[2] == "Charles Casper Stockham":
+            StockhamVotes += 1
+
+        elif row[2] == "Diana DeGette":
+            DegetteVotes += 1
+
+
+        elif row[2] == "Raymon Anthony Doane" :
+            DoaneVotes += 1
+
+        
+    if StockhamVotes > DegetteVotes and StockhamVotes > DoaneVotes :
+        winner = "Charles Casper Stockham"
+    elif DegetteVotes > DoaneVotes :
+        winner = "Diana DeGette"
+    else:
+         winner = "Raymon Anthony Doane"     
+        
+            
+
+
+    
+
+
+print(f"\n\tElection Results \n\n \t-------------------- \n")
+print(f"\tTotal Votes : {totalVotes}\n\n \t-------------------- \n")
+print(f"\tCharles Casper Stockham : {(StockhamVotes/totalVotes) * 100 }% ({StockhamVotes})\n")
+print(f"\tDiana DeGette : {(DegetteVotes/totalVotes) * 100}% ({DegetteVotes})\n")
+print(f"\tRaymon Anthony Doane : {(DoaneVotes/totalVotes) * 100}% ({DoaneVotes})\n\n \t-------------------- \n")
+
+print(f"\tWinner : {winner}\n\n \t-------------------- \n")
+
+
+
+
+
+
+
